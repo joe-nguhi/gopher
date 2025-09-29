9c34de5f07b1333df587cbedc17b7c02c77fe117
Nguhi <nguhi.dev@gmail.com>
2025-09-29 17:19:55 +0300

    :sparkles: implement a procedure, similar to Unix uniq to find duplicate lines in a file

--- main.go
+++ main.go
@@ -1,8 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 // Learning Go K&D Book
 func main() {
-	fmt.Println("Hello World")
+	//fmt.Println("Hello World")
+
+	files := os.Args[1:]
+	dup1(files)
+}
+
+// Unix uniq
+// dup1 Find duplicate lines in given files -v1
+func dup1(files []string) {
+	stats := make(map[string]map[string]int)
+
+	for _, file := range files {
+		c := make(map[string]int)
+
+		f, err := os.Open(file)
+		if err != nil {
+			return
+		}
+
+		scanner := bufio.NewScanner(f)
+
+		for scanner.Scan() {
+			c[scanner.Text()]++
+		}
+
+		stats[file] = c
+
+		f.Close()
+	}
+
+	printResults(stats)
+}
+
+func printResults(stats map[string]map[string]int) {
+	fmt.Println("------------ Duplicates Summary -------------")
+
+	for name, counter := range stats {
+		fmt.Printf("Filename: \t%s\n", name)
+		fmt.Println("Count: \t Line")
+		fmt.Println(strings.Repeat("---", 10))
+		for line, c := range counter {
+			if c > 1 {
+				fmt.Printf("%d\t%s\n", c, line)
+			}
+		}
+	}
 }
