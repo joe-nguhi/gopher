01d7d6f7ba3e7d893dc2ba6139a1dc0da1f04ddb
Nguhi <nguhi.dev@gmail.com>
2025-09-30 19:04:12 +0300

    refactor(nguhi.dev/gopher): move Lissajous function to animatedgif package

---
+++ animatedgif/lissajous.go
@@ -0,0 +1,81 @@
+package animatedgif
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+)
+
+var palette = []color.Color{color.Black,
+	color.RGBA{
+		R: 0,
+		G: 100,
+		B: 0,
+		A: 1,
+	},
+	color.RGBA{
+		R: 100,
+		G: 90,
+		B: 10,
+		A: 1,
+	}, color.RGBA{
+		R: 70,
+		G: 100,
+		B: 50,
+		A: 1,
+	},
+	color.RGBA{
+		R: 0,
+		G: 100,
+		B: 150,
+		A: 1,
+	},
+}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func Lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.005 // angular resolution
+		size    = 200   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 10    // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 1.5 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				//blackIndex,
+				uint8(rand.Intn(5)),
+			)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+
+	err := gif.EncodeAll(out, &anim)
+
+	if err != nil {
+		log.Println(fmt.Errorf("error: %s", err))
+		return
+	} // NOTE: ignoring encoding errors
+}
--- main.go
+++ main.go
@@ -3,47 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"image"
-	"image/color"
-	"image/gif"
 	"io"
-	"log"
-	"math"
-	"math/rand"
 	"net/http"
 	"os"
 	"strings"
-)
 
-var palette = []color.Color{color.Black,
-	color.RGBA{
-		R: 0,
-		G: 100,
-		B: 0,
-		A: 1,
-	},
-	color.RGBA{
-		R: 100,
-		G: 90,
-		B: 10,
-		A: 1,
-	}, color.RGBA{
-		R: 70,
-		G: 100,
-		B: 50,
-		A: 1,
-	},
-	color.RGBA{
-		R: 0,
-		G: 100,
-		B: 150,
-		A: 1,
-	},
-}
-
-const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	"nguhi.dev/gopher/animatedgif"
 )
 
 const httpsScheme = "https://"
@@ -57,22 +22,19 @@ func main() {
 	//dup1(files)
 
 	//Lissajous Figures
-	/*file, err := os.Create("lissajous.gif")
+	file, err := os.Create("lissajous.gif")
 
 	if err != nil {
-		log.Fatalf("%v", err)
-		//log.Println(fmt.Errorf("error: %s", err))
-		//file = os.Stdout
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
 	}
 
-	//fmt.Printf("File: %v\n,", file)
-
-	lissajous(file)*/
+	animatedgif.Lissajous(file)
 
 	// Fetch Url
-	links := os.Args[1:]
+	/*	links := os.Args[1:]
 
-	fetchUrls(links)
+		fetchUrls(links)*/
 }
 
 func fetchUrls(links []string) {
@@ -104,42 +66,6 @@ func fetchUrls(links []string) {
 		fmt.Printf("Link: %s:\t Status:%s\n", link, response.Status)
 		_, err = io.Copy(os.Stderr, response.Body)
 	}
-}
-
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.005 // angular resolution
-		size    = 200   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 10    // delay between frames in 10ms units
-	)
-
-	freq := rand.Float64() * 1.5 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
-
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				uint8(rand.Intn(5)))
-		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
-	}
-
-	err := gif.EncodeAll(out, &anim)
-
-	if err != nil {
-		log.Println(fmt.Errorf("error: %s", err))
-		return
-	} // NOTE: ignoring encoding errors
 }
 
 // Unix uniq
