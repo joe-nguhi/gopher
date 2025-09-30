20d51503989e45c844b76a2b88bbed8c92c2f17f
Nguhi <nguhi.dev@gmail.com>
2025-09-30 23:01:34 +0300

    refactor(main): clean up code

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
-import (
-	"os"
-
-	"nguhi.dev/gopher/fetch"
-)
-
 // Learning Go K&D Book
 func main() {
 
 	// find duplicates
 	/*
 		files := os.Args[1:]
-		uniq.Dup1(files)
+		//uniq.Dup1(files)
 		uniq.Dup2(files)
 	*/
 
 	//Lissajous Figures
 	/*
-		file, err := os.Create("lissajous.gif")
-
+		file, err := os.Create("assets/lissajous.gif")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v", err)
 			os.Exit(1)
@@ -28,7 +21,9 @@ func main() {
 	*/
 
 	// Fetch Url
-	links := os.Args[1:]
-	fetch.SequentialFetch(links)
-	fetch.ParallelFetch(links)
+	/*
+		links := os.Args[1:]
+		fetch.SequentialFetch(links)
+		fetch.ParallelFetch(links)
+	*/
 }
